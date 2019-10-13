9961b187446204784b457411d708e5f1bc42972e
arun-kmr-singh <parmaraks1@gmail.com>
2019-10-14 04:09:55 +0530

    new commit for calcy.py

---
+++ calc.py
@@ -0,0 +1,20 @@
+def add(a,b):
+	return a+b
+
+def sub(a,b):
+	return a-b
+
+def mul(a,b):
+	return a*b
+
+def div(a,b):
+	return a/b
+
+print("Enter the number:")
+a=int(input("a:"))
+b=int(input("b:"))
+
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
